82c3d47fbdead216ec9ce3a265832e4424c54a71
Garchbold <garchbold@g.ucla.edu>
2016-08-18 21:05:36 +0200

    initial lights funciton

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 /**
  * Created by Daniel on 8/9/2016.
  */
-(function(){
+(
+
+	function neonlights(){
     //Neon lights effect: George
-});
+    	var indexleft = document.getElementById('left');
+    	
+
+    	if(indexleft.style.color == "yellow"){
+    		indexleft.style.color = "navy";
+    	}
+    	else{
+    		indexleft.style.color = "yellow";
+    	}
+	}
+);
