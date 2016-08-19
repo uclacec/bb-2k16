5684a4e67ce732cf1c17d8f88d409e34d5a306a2
Garchbold <garchbold@g.ucla.edu>
2016-08-19 06:37:10 +0200

    initial color scheme change

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
 /**
- * Created by Daniel on 8/9/2016.
+ * Created by George on 8/9/2016.
  */
-(function(){
-    //Neon lights effect: George
+
+
+
+$(document).ready(function(){
+
+	var lights = false;
+
+
+	if(lights == false){
+		$('#left').on('click', function(){ //here is the trigger div, should be changed
+			
+			lights = true;
+
+			$('#left').css({ "background-color" : "#000066"});
+ 			$('#concert').css({ "background-color" : "#0000cc"});
+ 			$('#film').css({ "background-color" : "#9999e6"});
+ 			$('#festival').css({ "background-color" : "#ff66d9"});
+ 			$('#faq').css({ "background-color" : "##00ffcc"});
+ 			$('.title').css({ "color" : "white"});
+ 			});
+		}
+	else{
+		$('#left').on('click', function(){ 
+			lights = false;
+			$('#left').css({ "background-color" : "yellow"});
+			//change everything back
+			 	});
+		}
 });
