fedc584a10407cbfc8cc0e50cdd52613db13ef78
Garchbold <garchbold@g.ucla.edu>
2016-08-19 09:01:55 +0200

    lights function for main page

--- scripts.js
+++ scripts.js
@@ -8,25 +8,26 @@ $(document).ready(function(){
 
 	var lights = false;
 
+	$('#left').on('click', function(){ //here is the trigger div, should be changed
 
 	if(lights == false){
-		$('#left').on('click', function(){ //here is the trigger div, should be changed
-			
-			lights = true;
-
-			$('#left').css({ "background-color" : "#000066"});
- 			$('#concert').css({ "background-color" : "#0000cc"});
- 			$('#film').css({ "background-color" : "#9999e6"});
- 			$('#festival').css({ "background-color" : "#ff66d9"});
- 			$('#faq').css({ "background-color" : "##00ffcc"});
- 			$('.title').css({ "color" : "white"});
- 			});
+		lights = true;
+		$('#left').css({ "background-color" : "#000066"});
+		$('#concert').css({ "background-color" : "#0000cc"});
+		$('#film').css({ "background-color" : "#9999e6"});
+		$('#festival').css({ "background-color" : "#ff66d9"});
+		$('#faq').css({ "background-color" : "#00ffcc"});
+		$('.title').css({ "color" : "white"});
 		}
-	else{
-		$('#left').on('click', function(){ 
-			lights = false;
-			$('#left').css({ "background-color" : "yellow"});
+	else if (lights == true) {
+		lights = false;
+		$('#left').css({ "background-color" : "yellow"});
+		$('#concert').css({ "background-color" : "deepskyblue"});
+		$('#film').css({ "background-color" : "lightcoral"});
+		$('#festival').css({ "background-color" : "#2ecc71"});
+		$('#faq').css({ "background-color" : "#9b59b6"});
+		$('.title').css({ "color" : "black"});
 			//change everything back
-			 	});
 		}
+	});
 });
